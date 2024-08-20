bd6983d7e398de83e51510e5ea71e1c6e5e5e8ca
hala ibrahim <halamamoun9@gmail.com>
2024-08-20 10:34:13 +0300

    first exercise

---
+++ exercise1.js
@@ -0,0 +1,15 @@
+function combine1(){
+    console.log(2*2/2);
+    
+}
+
+
+combine1();
+
+function combine(a,b,c){
+    return a*b/c;
+    
+}
+
+const result=combine(2,3,4);
+console.log(result);
